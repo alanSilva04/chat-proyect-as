3e106e7a679add6274a096675e8a9a988cd24579
Alan <alan@Contrasenas-MacBook-Pro.local>
2022-08-26 11:10:29 -0300

    api allowed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 import { useState } from "react";
 import Chat from "./Chat"
 
-const socket = io.connect("https://chat-io-project.herokuapp.com/");
+const socket = io.connect("https://chat-project-api.herokuapp.com/");
 
 function App() {
   const [username, setUsername] = useState("");
